e79379ee5af1d0dc333e5acdc3bf8f6106f11bd1
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2023-10-24 13:22:07 +0300

    Firebase Libraries added

--- app/src/main/java/com/hdogmbh/budgettracker/RegistrationActivity.java
+++ app/src/main/java/com/hdogmbh/budgettracker/RegistrationActivity.java
@@ -1,7 +1,9 @@
 package com.hdogmbh.budgettracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,6 +11,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -16,12 +24,18 @@ public class RegistrationActivity extends AppCompatActivity {
     private EditText mPasswordRegister;
     private Button btnRegister;
     private TextView haveAccount;
+    private ProgressDialog mDialog;
+    //Firebase
+    private FirebaseAuth mAuth;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        mAuth = FirebaseAuth.getInstance();
+        mDialog = new ProgressDialog(this,0);
         register();
     }
     private void register(){
@@ -40,7 +54,20 @@ public class RegistrationActivity extends AppCompatActivity {
                     mEmailRegister.setError("Email and Password are required");
                     return;
                 }
-
+                mDialog.setMessage("Processing");
+                mAuth.createUserWithEmailAndPassword(emailRegister,passwordRegister).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            //Redirect to home page
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(),"Registration complete",Toast.LENGTH_SHORT).show();
+                        } else {
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(),"Registration failed",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
             }
         });
         haveAccount.setOnClickListener(new View.OnClickListener() {
