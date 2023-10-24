5c097c6757a0287813ef412be815c73f8ab7bf53
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2023-10-24 14:44:31 +0300

    login success

--- app/src/main/java/com/hdogmbh/budgettracker/MainActivity.java
+++ app/src/main/java/com/hdogmbh/budgettracker/MainActivity.java
@@ -1,7 +1,9 @@
 package com.hdogmbh.budgettracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,6 +11,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +25,16 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
     private TextView newAccount;
     private TextView forgotPassword;
+    private ProgressDialog mDialog;
+    //Firebase
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDialog = new ProgressDialog(this);
         login();
     }
     private void login(){
@@ -30,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btn_login);
         newAccount = findViewById(R.id.new_account);
         forgotPassword = findViewById(R.id.forget_password);
+        mAuth = FirebaseAuth.getInstance();
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +55,21 @@ public class MainActivity extends AppCompatActivity {
                     mEmailLogin.setError("Email and Password are required");
                     return;
                 }
+                mDialog.setMessage("Processing");
+                mDialog.show();
+                mAuth.signInWithEmailAndPassword(emailLogin, passwordLogin).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            mDialog.dismiss();
+                            Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                        } else{
+                            mDialog.dismiss();
+                            Toast.makeText(MainActivity.this, "Login NOT Successful", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
         // register new account and intent
