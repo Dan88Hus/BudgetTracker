f04e913e71331d068151baa712612c458bacdfc7
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2023-10-24 09:51:43 +0300

    login and Register screen added

---
+++ app/src/main/java/com/hdogmbh/budgettracker/MainActivity.java
@@ -0,0 +1,46 @@
+package com.hdogmbh.budgettracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mEmailLogin;
+    private EditText mPasswordLogin;
+    private Button btnLogin;
+    private TextView newAccount;
+    private TextView forgotPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login();
+    }
+    private void login(){
+        mEmailLogin = findViewById(R.id.email_login);
+        mPasswordLogin = findViewById(R.id.password_login);
+        btnLogin = findViewById(R.id.btn_login);
+        newAccount = findViewById(R.id.new_account);
+        forgotPassword = findViewById(R.id.forget_password);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailLogin = mEmailLogin.getText().toString().trim();
+                String passwordLogin = mPasswordLogin.getText().toString().trim();
+                if (TextUtils.isEmpty(emailLogin)||TextUtils.isEmpty(passwordLogin)){
+                    //can be added Toast message in here.
+                    mEmailLogin.setError("Email and Password are required");
+                    return;
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/hdogmbh/budgettracker/RegistrationActivity.java
@@ -0,0 +1,49 @@
+package com.hdogmbh.budgettracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText mEmailRegister;
+    private EditText mPasswordRegister;
+    private Button btnRegister;
+    private TextView haveAccount;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        register();
+    }
+    private void register(){
+        mEmailRegister = findViewById(R.id.email_register);
+        mPasswordRegister = findViewById(R.id.password_register);
+        btnRegister = findViewById(R.id.btn_register);
+        haveAccount = findViewById(R.id.have_account);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailRegister = mEmailRegister.getText().toString().trim();
+                String passwordRegister = mPasswordRegister.getText().toString().trim();
+                if (TextUtils.isEmpty(emailRegister)||TextUtils.isEmpty(passwordRegister)){
+                    //can be added Toast message in here.
+                    mEmailRegister.setError("Email and Password are required");
+                    return;
+                }
+
+            }
+        });
+
+
+
+    }
+}
